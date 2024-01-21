901f2e0c57c7e75aed12f908657974a66ca1a5ec
ChibaTomoki <michiamo.t1@gmail.com>
2024-01-21 16:34:43 +0900

    feat: supabase dbから値を取得できるように

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../database.types";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+export const supabase = createClient<Database>(
+  import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  import.meta.env.VITE_SUPABASE_API_KEY
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
