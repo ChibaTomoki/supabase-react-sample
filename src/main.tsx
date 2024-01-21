ef1449bcf736375aec67830849fb4129afbb2cf5
ChibaTomoki <michiamo.t1@gmail.com>
2024-01-21 17:18:39 +0900

    add: react routerの導入とそのサンプルの追加

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,27 @@ import App from "./App.tsx";
 import "./index.css";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../database.types";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Page1 from "./pages/page1.tsx";
 
 export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_PROJECT_URL,
   import.meta.env.VITE_SUPABASE_API_KEY
 );
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/page1",
+    element: <Page1 />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
